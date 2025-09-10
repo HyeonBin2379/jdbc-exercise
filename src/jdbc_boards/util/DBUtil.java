2d59a8cb4ce5bbe9cd0e22fec5c5c332a7fd2590
Lee Hyeon Bin <hyeonbin2379@gmail.com>
2025-09-10 10:45:31 +0900

    vo 및 util 패키지 수정

--- src/jdbc_boards/util/DBUtil.java
+++ src/jdbc_boards/util/DBUtil.java
@@ -3,17 +3,16 @@ package jdbc_boards.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class DBUtil {
 
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/bookmarketdb?serverTimezone=Asia/Seoul";
-    private static final String USERNAME = "bookadmin";
-    private static final String PASSWORD = "bookadmin";
+    private static ResourceBundle bundle;
 
     static {
+        bundle = ResourceBundle.getBundle("util.dbinfo");
         try {
-            Class.forName(DRIVER);
+            Class.forName(bundle.getString("driver"));
             System.out.println("드라이버 로딩 성공");
         } catch (ClassNotFoundException e) {
             System.out.println("드라이버 로딩 실패");
@@ -23,7 +22,11 @@ public class DBUtil {
 
     public static Connection getConnection() {
         try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            return DriverManager.getConnection(
+                    bundle.getString("url"),
+                    bundle.getString("username"),
+                    bundle.getString("password")
+            );
         } catch(SQLException e) {
             throw new RuntimeException(e);
         }
--- src/jdbc_boards/vo/Board.java
+++ src/jdbc_boards/vo/Board.java
@@ -4,10 +4,6 @@ import lombok.*;
 
 import java.util.Date;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @ToString
 public class Board {
 
@@ -17,11 +13,61 @@ public class Board {
     private String bWriter;
     private Date bDate;
 
+    public Board() {}
+
+    public Board(int bno, String bTitle, String bContent, String bWriter, Date bDate) {
+        this.bno = bno;
+        this.bTitle = bTitle;
+        this.bContent = bContent;
+        this.bWriter = bWriter;
+        this.bDate = bDate;
+    }
+
+    public int getBno() {
+        return bno;
+    }
+
+    public void setBno(int bno) {
+        this.bno = bno;
+    }
+
+    public String getbTitle() {
+        return bTitle;
+    }
+
+    public void setbTitle(String bTitle) {
+        this.bTitle = bTitle;
+    }
+
+    public String getbContent() {
+        return bContent;
+    }
+
+    public void setbContent(String bContent) {
+        this.bContent = bContent;
+    }
+
+    public String getbWriter() {
+        return bWriter;
+    }
+
+    public void setbWriter(String bWriter) {
+        this.bWriter = bWriter;
+    }
+
+    public Date getbDate() {
+        return bDate;
+    }
+
+    public void setbDate(Date bDate) {
+        this.bDate = bDate;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o instanceof Board that){
             return bno == that.bno;
         }
-        throw new IllegalArgumentException("비교할 대상이 올바르지 않습니다.");
+        throw new ClassCastException("비교할 대상의 타입이 올바르지 않습니다.");
     }
 }
