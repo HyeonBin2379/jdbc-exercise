eefe5ecbe62f95d96ca0fc17210e8a00dd187c4c
Lee Hyeon Bin <hyeonbin2379@gmail.com>
2025-09-24 19:11:29 +0900

    뒤로가기 기능 수정

--- src/exercise_v1/controller/MemberMenu.java
+++ src/exercise_v1/controller/MemberMenu.java
@@ -1,20 +1,77 @@
 package exercise_v1.controller;
 
+import exercise_v1.constant.MemberManagement;
+import exercise_v1.domain.Member;
 import exercise_v1.domain.User;
+import exercise_v1.model.MemberDAO;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class MemberMenu {
 
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+    private final MemberDAO dao;
     private final User user;
 
-    public MemberMenu(User user) {
-        this.user = user;
+    private boolean quitMenu;
+
+    public MemberMenu(User loginUser) {
+        this.dao = new MemberDAO();
+        this.user = loginUser;
+        this.quitMenu = false;
     }
 
     public void run() {
+        while (!quitMenu) {
+            try {
+                printMenu();
+                String menuNum = input.readLine();
+                switch (menuNum) {
+                    case "1":
+                        select();
+                        break;
+                    case "2":
+                        update();
+                        break;
+                    case "3":
+                        delete();
+                        break;
+                    case "4":
+                        exitMenu();
+                        break;
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
 
+        }
+    }
+
+    public void printMenu() {
+        if (user instanceof Member) {
+            System.out.println(MemberManagement.MEMBER_MEMBER_MENU_TITLE);
+        } else {
+            System.out.println(MemberManagement.MANAGER_MEMBER_MENU_SEARCH);
+        }
+    }
+
+    public void select() {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void delete() {
+
+    }
+
+    public void exitMenu() {
+        quitMenu = true;
+        System.out.println(MemberManagement.MEMBER_MENU_PREVIOUS);
     }
 }
--- src/exercise_v1/controller/WMSMenu.java
+++ src/exercise_v1/controller/WMSMenu.java
@@ -13,11 +13,11 @@ import java.io.InputStreamReader;
 public class WMSMenu {
 
     private static final BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-    private static boolean quitWMS;
-
-    private MemberMenu memberMenu;
 
     private final User currentLoginUser;
+    private boolean quitWMS;
+
+    private MemberMenu memberMenu;
 
     public WMSMenu(User loginUser) {
         this.currentLoginUser = loginUser;
@@ -25,34 +25,12 @@ public class WMSMenu {
 
     public void run() {
         quitWMS = false;
-        if (currentLoginUser instanceof Member member) {
-            memberMenu(member);
-        } else if (currentLoginUser instanceof Manager manager) {
-            managerMenu(manager);
-        }
-    }
-
-    public void memberMenu(Member member) {
         while (!quitWMS) {
             try {
-                System.out.print(WMSMessage.MEMBER_MENU_TITLE);
-                String menuNum = input.readLine();
-                switch (menuNum) {
-                    case "1":   // 회원관리
-                        memberMenu = new MemberMenu(member);
-                        memberMenu.run();
-                        break;
-                    case "2":   // 고객센터
-                        break;
-                    case "3":   // 재고관리
-                        break;
-                    case "4":   // 입고
-                        break;
-                    case "5":   // 출고
-                        break;
-                    case "6":   // 로그아웃
-                        logout(member.getId());
-                        break;
+                if (currentLoginUser instanceof Member member) {
+                    memberMenuList(member);
+                } else if (currentLoginUser instanceof Manager manager) {
+                    managerMenuList(manager);
                 }
             } catch (IOException e) {
                 System.out.println(e.getMessage());
@@ -60,35 +38,54 @@ public class WMSMenu {
         }
     }
 
-    public void managerMenu(Manager manager) {
+    public void memberMenuList(Member member) throws IOException {
+        System.out.print(WMSMessage.MEMBER_MENU_TITLE);
+        String menuNum = input.readLine();
+        switch (menuNum) {
+            case "1":   // 회원관리
+                memberManagement(member);
+                break;
+            case "2":   // 고객센터
+                break;
+            case "3":   // 재고관리
+                break;
+            case "4":   // 입고
+                break;
+            case "5":   // 출고
+                break;
+            case "6":   // 로그아웃
+                logout(member.getId());
+                break;
+        }
+    }
+
+    public void managerMenuList(Manager manager) throws IOException {
         // 관리자 전용 기능이 존재하여 memberMenu(), managerMenu()를 구분
-        while (!quitWMS) {
-            try {
-                System.out.print(WMSMessage.MANAGER_MENU_TITLE);
-                String menuNum = input.readLine();
-                switch (menuNum) {
-                    case "1":   // 회원관리
-                        memberMenu = new MemberMenu(manager);
-                        memberMenu.run();
-                        break;
-                    case "2":   // 고객센터
-                        break;
-                    case "3":   // 창고관리
-                        break;
-                    case "4":   // 재고관리
-                        break;
-                    case "5":   // 입고
-                        break;
-                    case "6":   // 출고
-                        break;
-                    case "7":   // 로그아웃
-                        logout(manager.getId());
-                        break;
-                }
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
+        System.out.print(WMSMessage.MANAGER_MENU_TITLE);
+        String menuNum = input.readLine();
+        switch (menuNum) {
+            case "1":   // 회원관리
+                memberManagement(manager);
+                break;
+            case "2":   // 고객센터
+                break;
+            case "3":   // 창고관리
+                break;
+            case "4":   // 재고관리
+                break;
+            case "5":   // 입고
+                break;
+            case "6":   // 출고
+                break;
+            case "7":   // 로그아웃
+                logout(manager.getId());
+                break;
         }
+    }
+
+    public void memberManagement(User user) {
+        memberMenu = new MemberMenu(user);
+        memberMenu.run();
     }
 
     public void logout(String userID) {
